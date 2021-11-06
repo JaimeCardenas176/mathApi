7340cf53335a7caadd86a2d029f6b2ae019754d0
JaimeCardenas176 <jaime.cardenas176@gmail.com>
2021-11-06 13:03:02 +0100

    some error handling

--- app.js
+++ app.js
@@ -6,14 +6,23 @@ const fibonacci = require('./routes/fibonacci');
 const operations = require('./routes/operations');
 const PREFIX = '/mathApi/v1/';
 
+const { logErrors, wrapErrors, errorHandler} = require('./utils/middlewares/errorHandler');
+const notFoundHandler = require('./utils/middlewares/notFoundHandler');
+
 const app = express();
 
-
 app.use(cookieParser())
+app.use(express.json());
 
 app.use(`${PREFIX}`, arrays);
 app.use(`${PREFIX}`, fibonacci);
 app.use(`${PREFIX}`, operations);
 
+app.use(notFoundHandler);
+
+//los middlewares de error tienen que ir tab al final (las rutas tambien son middlewares)
+app.use(logErrors);
+app.use(wrapErrors);
+app.use(errorHandler);
 
 module.exports = app;
